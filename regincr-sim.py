a333815861eef94c3ff5f70d62071cea2292ec80
Christopher Batten <cbatten@cornell.edu>
2015-01-31 15:52:09 -0500

    update code snippets

---
+++ regincr-sim.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#=========================================================================
+# regincr-sim-v1 <input-values>
+#=========================================================================
+
+from pymtl   import *
+from sys     import argv
+from RegIncr import RegIncr
+
+# Get list of input values from command line
+
+input_values = [ int(x,0) for x in argv[1:] ]
+
+# Add three zero values to end of list of input values
+
+input_values.extend( [0]*3 )
+
+# Elaborate the model
+
+model = RegIncr()
+model.elaborate()
+
+# Create a simulator using simulation tool
+
+sim = SimulationTool( model )
+
+# Reset simulator
+
+sim.reset()
+
+# Apply input values and display output values
+
+for input_value in input_values:
+
+  # Write input value to input port
+
+  model.in_.value = input_value
+
+  # Display input and output ports
+
+  print " cycle = {}: in = {:02x}, out = {}" \
+    .format( sim.ncycles, model.in_, model.out )
+
+  # Tick simulator one cycle
+
+  sim.cycle()
+
